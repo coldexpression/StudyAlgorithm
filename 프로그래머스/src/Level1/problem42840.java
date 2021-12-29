3c98189116650ab1fca17a1a5ebe974ff143bb1d
coldexpression <uni_vers_@naver.com>
2021-12-29 20:23:04 +0900

    [정답] 42840번 (케이스 오류 해결)

--- 프로그래머스/src/Level1/problem42840.java
+++ 프로그래머스/src/Level1/problem42840.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class problem42840 {
     public static void main(String[] args) {
         problem42840 problem42840 = new problem42840();
-        int[] answer = problem42840.solution(new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5});
+        int[] answer = problem42840.solution(new int[]{1,2,3,4,5});
     }
 
     public int[] solution(int[] answers) {
@@ -18,72 +18,56 @@ public class problem42840 {
         int[] two = {2,1,2,3,2,4,2,5};
         int[] three = {3,3,1,1,2,2,4,4,5,5};
         List<Integer> index = new ArrayList<>();
-        List<Integer> values = new ArrayList<>();
         int[] answer = {};
+        int[] count = new int[3];
         int one_size = one.length;
         int two_size = two.length;
         int three_size = three.length;
-        int one_data = 0;
-        int two_data = 0;
-        int three_data = 0;
-        int one_count = 0;
-        int two_count = 0;
-        int three_count = 0;
-        int cnt= 0;
         int max = 0;
-        int max_index= 0;
 
         if (answers.length == 0) {
             return answer;
         }
+
         for(int i=0;i<answers.length;i++) {
 
-            System.out.println(i%one_size);
-            System.out.println(i%two_size);
-            System.out.println(i%three_size);
-
-            one_count = one[i%one_size] == answers[i] ? one_count+1 : one_count;
-            two_count = two[i%two_size] == answers[i] ? two_count+1 : two_count;
-            three_count = three[i%three_size] == answers[i] ? three_count+1: three_count;
+            count[0] = one[i%one_size] == answers[i] ? count[0]+1 :count[0];
+            count[1] = two[i%two_size] == answers[i] ? count[1]+1 : count[1];
+            count[2] = three[i%three_size] == answers[i] ? count[2]+1: count[2];
         }
 
-
-        if (one_count != 0) {
-            index.add(1);
-            values.add(one_count);
-        }
-
-        if (two_count != 0) {
-            index.add(2);
-            values.add(two_count);
-        }
-
-        if (three_count != 0) {
-            index.add(3);
-            values.add(three_count);
-        }
-
-        if (index.size() != 0) {
-            answer = new int[index.size()];
-        } else {
+        if (count[0] == count[1] && count[1] == count[2]) {
+            answer = new int[3];
+            answer[0] = 1;
+            answer[1] = 2;
+            answer[2] = 3;
             return answer;
         }
 
+        max = -1;
 
-        while(index.size() != 0) {
-            max = values.get(0);
-            max_index = 0;
-            for (int i=1;i<index.size();i++) {
-                if (max < values.get(i)) {
-                    max = values.get(i);
-                    max_index = i;
-                }
+        for(int i=0;i<count.length;i++) {
+            if (max < count[i]) {
+                max = count[i];
             }
-            answer[cnt] = index.get(max_index);
-            index.remove(max_index);
-            values.remove(max_index);
-            cnt++;
         }
+
+
+        for(int i=0;i<count.length;i++) {
+            if (max == count[i]) {
+                index.add(i+1);
+            }
+        }
+
+        for(int i=0;i<index.size();i++) {
+        }
+
+        answer = new int[index.size()];
+
+        for(int i=0;i<index.size();i++) {
+            answer[i] = index.get(i);
+        }
+
 
 
         return answer;
