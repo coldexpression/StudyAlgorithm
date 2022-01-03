7c0330ccc052b15290b343cc59f9c2b4e38e2fa6
coldexpression <uni_vers_@naver.com>
2022-01-03 14:01:02 +0900

    [정답] 12943번

--- 프로그래머스/src/Level1/problem12943.java
+++ 프로그래머스/src/Level1/problem12943.java
@@ -8,7 +8,6 @@ public class problem12943 {
 
     public int solution(long num) {
         int answer = 0;
-        int tmp = 0;
         while(num != 1) {
             if (answer >= 500) return -1;
             num = num % 2 == 0 ? num / 2 : (num*3)+1;
