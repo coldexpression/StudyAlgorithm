d1d597e2071bd3b7588c9b10c82ca1883579865a
coldexpression <uni_vers_@naver.com>
2022-01-04 20:03:36 +0900

    [정답] 12937번

---
+++ 프로그래머스/src/Level1/problem12937.java
@@ -0,0 +1,14 @@
+package Level1;
+
+public class problem12937 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public String solution(int num) {
+        String answer = "";
+        answer = num % 2 == 0 ? "Even" : "Odd";
+        return answer;
+    }
+}
