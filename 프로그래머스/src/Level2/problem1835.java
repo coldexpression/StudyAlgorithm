47b0099e148f3fa76c3ce52050122351bd31919f
coldexpression <uni_vers_@naver.com>
2022-01-11 22:52:00 +0900

    [풀이 중 - DFS 사용] 1835번

--- 프로그래머스/src/Level2/problem1835.java
+++ 프로그래머스/src/Level2/problem1835.java
@@ -5,85 +5,82 @@ import java.util.List;
 
 public class problem1835 {
 
-    public static void main(String[] args) {
+    static String[] photoZone = {"0","0","0","0","0","0","0","0"};
+    static String[] people = {"A","C","F","J","M","N","R","T"};
+    static int count = 0;
+    static int answer = 0;
 
+    public static void main(String[] args) {
+        problem1835 problem1835 = new problem1835();
+        problem1835.solution(2, new String[]{"N~F=0","R~T>2"});
     }
 
-    public int solution(int n, String[] data) {
-        int answer = 0;
-        // {N,F,R,A,C,T,J,M} => 64 // (16+8+8+8) => 40 // (16+8+4+2) => 30
-        // acjm acmj ajcm ajmc amcj amjc
-        // cajm camj cjam cjma cmaj cmja
-        // jacm jamc jcam jcma jmac jmca
-        // macj majc mcaj mcja mjac mjca
-        // 8+4+4+4 = 20
-        // {N,F,R,A,C,J,T,M} => 64 // 40 // 20
-        // {N,F,R,A,C,J,M,T} => 64 // 40 // 20
-        // {N,F,A,R,C,J,T,M} => 64 // 40 // 20
-        // {N,F,A,R,C,J,M,T} => 64 // 40 // 20
-        // {N,F,A,C,R,J,M,T} => 64 // 40 // 20
-        // => 64*6 = 384 // 40* 6 => 240 // 30* 6 * 2 => 360
-        // R 과 T 위치 변경 => 384*2 = 768 // 480 // 20 * 6 * 2 = 240
-        // {R,N,F,T,A,C,J,M} => 64 // 16 * 5 * 2 => 160
-        // {R,N,F,A,T,C,J,M} => 64
-        // {R,N,F,A,C,T,J,M} => 64
-        // {R,N,F,A,C,J,T,M} => 64
-        // {R,N,F,A,C,J,M,T} => 64
-        //  768 * 2 => 1536 // 40 * 5 => 200 * 2 => 400 // 30 * 5 * 2 => 300 // 20 * 5 * 2 => 200
-        // {R,A,N,F,T,C,J,M} => 64 // 16 * 4 * 2 => 128
-        // ... 64*4 = 256 * 2 = 512 // 40*4 => 160 * 2 => 320 // 30 * 4 * 2 => 240 // 20 * 4 * 2 => 160
-        // 1536 + 512 = 2048
-        // {R,A,C,N,F,T,J,M} => 64 // 16 * 3 * 2 => 96
-        // ... 64 * 3 => 192 * 2 => 384 // 40 * 3 * 2 => 240 // 30 * 3 * 2= > 180 // 20 * 3 * 2 => 120
-        // 2048 + 384 = 2432
-        // ... 64 * 3 => 252 * 2 => 504 // 40 * 3 => 120 * 2 => 240 // 30 * 3 * 2 => 180 // 20 * 3* 2 => 120
-        // 2432 + 504 = 2936
-        // {R,A,C,T,N,F,J,M}
-        // {R,A,C,T,J,N,F,M}
-        // {R,A,C,T,J,M,N,F}
-        // {R,A,C,J,N,F,M,T} => 64 // 16 * 2 * 2 => 64
-        // ... 64 * 2 => 128 * 2 => 256 // 40 * 2 * 2 => 160 // 30 * 2 * 2 => 120 // 20 * 2 * 2 => 80
-        // 2936 + 256 = 3192
-        // {R,A,C,J,M,N,F,T}
-        // {R,A,C,J,M,T,N,F}
+    public void solution(int n, String[] data) {
+        count = 0;
+        answer = 0;
+        dfs(0,n,data);
+        System.out.println(answer);
+    }
 
-        // {R,A,C,J,M,T,N,F} => 64 // 16 * 2 => 32
-        // ... 64 * 2 => 128 // 40 * 2 => 80 // 30 * 2 => 60 // 20 * 2 => 40
-        // 3192 + 128 => 3320
-        String[] store = new String[]{"A","C","F","J","M","N","R","T"};
-        String[] exp = new String[data.length];
-        int[] num = new int[data.length];
-        List<String> alpha = new ArrayList<>();
-        String[] temp = new String[8];
-        int count = 0;
-        int index = 0;
-        int length = 0;
-        for(int i=0;i<data.length;i++) {
-            exp[i] = data[i].split("")[data.length-2];
-            num[i] = Integer.parseInt(data[i].split("")[data.length-1]);
-            alpha.add(data[i].split("")[0]);
-            alpha.add(data[i].split("")[2]);
-        }
+    static void dfs(int index, int n, String[] data) {
+        if (count == 8) {
+            boolean[] check = new boolean[n];
+            int checkNum = 0;
+            for (int i=0;i<n;i++) {
+                check[i] = validation(data[i]);
+            }
 
-        while(true) {
-            length = num[count];
+            for (int i=0;i<n;i++) {
+                checkNum = check[i] ? checkNum + 1 : checkNum;
+            }
 
-            switch(exp[count]) {
-                case "=" : {
-                    temp[0] = alpha.get(0);
-                    alpha.remove(0);
-                    temp[length+1] = alpha.get(0);
-                    alpha.remove(0);
-                } break;
-                case "<" : {
-
-                } break;
-                case ">" : {
-
-                } break;
+            answer = checkNum == n ? answer + 1 : answer;
+        } else {
+            for(int i=0;i<8;i++) {
+                if (findIndex(i) == -1) {
+                    photoZone[index] =  people[i];
+                    dfs(index + 1, n, data);
+                    photoZone[index] = "0";
+                    count--;
+                }
             }
         }
+    }
 
-        return answer;
+    static int findIndex(int index) {
+        for(int i=0; i<people.length;i++) {
+            if (photoZone[i] == people[index]) return index;
+        }
+        count++;
+        return -1;
+    }
+
+    static boolean validation(String data) {
+        String temp = "";
+        for(String word: photoZone) temp += word;
+
+//        System.out.println(temp);
+
+        char s1 = data.charAt(0);
+        char s2 = data.charAt(2);
+        char exp = data.charAt(3);
+        int num = Integer.parseInt(String.valueOf(data.charAt(4)));
+        int distance = temp.indexOf(s1) - temp.indexOf(s2);
+
+        distance = distance > 0 ? distance : distance * (-1);
+
+        switch (exp) {
+            case '=' : {
+                if (distance == num) return true;
+            }
+            case '>' : {
+                if (distance > num) return true;
+            }
+            case '<' : {
+                if (distance < num) return true;
+            }
+            default: return false;
+        }
+
     }
 }
