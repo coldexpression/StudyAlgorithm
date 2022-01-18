53663d93fed3b768752a689bed102cd8de4e5a23
coldexpression <uni_vers_@naver.com>
2022-01-18 18:38:41 +0900

    [정답] 12973번

--- 프로그래머스/src/Level2/problem12973.java
+++ 프로그래머스/src/Level2/problem12973.java
@@ -1,48 +1,25 @@
 package Level2;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Stack;
 
 public class problem12973 {
 
     public static void main(String[] args) {
         problem12973 problem12973 = new problem12973();
-        problem12973.solution("cdcd");
+        problem12973.solution("baabaa");
     }
 
     public int solution(String s) {
         int answer = -1;
-        int length = 0;
-        int index = 0;
-        int count = 0;
-        boolean check;
-        char pivot;
-        List<Character> store = new ArrayList<>();
+        Stack<Character> stack = new Stack<>();
         char[] words = s.toCharArray();
-        for(char word: words) store.add(word);
-        while(!store.isEmpty()) {
-            check = false;
-            count = 0;
-            length = store.size();
-            if (index == length) {
-                answer = 0;
-                break;
-            }
-            pivot = store.get(index);
-            for (int i = index+1; i < length; i++) {
-                if (pivot == store.get(i)) count++;
-                else break;
-            }
-
-            if (count > 0) {
-                for(int i=0;i<=count;i++) store.remove(index);
-                check = true;
-            }
-
-            index = check ? 0 : index+1;
+        for(char word: words) {
+            if (stack.isEmpty() || stack.peek() != word) stack.push(word);
+            else if (stack.peek() == word) stack.pop();
         }
-        answer = answer == 0 ? 0 : 1;
 
+        answer = stack.size() == 0 ? 1 : 0;
         return answer;
     }
 }
