2c30ffd305c8efad6edd59c9d18168a06d8e47e0
coldexpression <uni_vers_@naver.com>
2022-02-10 23:31:35 +0900

    [정답- 다시한번 풀어보기] 67257번

--- 프로그래머스/src/Level2/problem67257.java
+++ 프로그래머스/src/Level2/problem67257.java
@@ -4,9 +4,11 @@ import java.util.*;
 
 public class problem67257 {
 
-    static String op = "OOO";
+    static List<Long> numStore = new ArrayList<>();
     static List<Character> opStore = new ArrayList<>();
-    static List<Long> sumStore = new ArrayList<>();
+    static List<Character> defaultOp = new ArrayList<>();
+    static long max = -1;
+    static boolean[] visited = new boolean[3];
 
     public static void main(String[] args) {
         problem67257 problem67257 = new problem67257();
@@ -14,172 +16,88 @@ public class problem67257 {
 //        System.out.println(test.charAt(4));
 //        System.out.println(test.indexOf("-"));
 //        System.out.println(test.indexOf("-",4));
-        problem67257.solution("177-661*999*99-133+221+334+555-166-144-551-166*166-166*166-133*88*55-11*4+55*888*454*12+11-66+444*99");
+        problem67257.solution("100-200*300-500+20");
     }
 
     public long solution(String expression) {
         long answer = 0;
-        long max = 0;
-        opStore.add('+');
-        opStore.add('-');
-        opStore.add('*');
-        dfs(expression, 0);
-        for(int i=0;i<sumStore.size();i++) {
-            max = Math.max(max, sumStore.get(i));
+        String num = "";
+        Arrays.fill(visited, false);
+        defaultOp.add('+');
+        defaultOp.add('-');
+        defaultOp.add('*');
+        for(int i=0;i<expression.length();i++){
+            if (expression.charAt(i) >= '0' && expression.charAt(i) <= '9') {
+                num = num + expression.charAt(i);
+            } else {
+                numStore.add(Long.parseLong(num));
+                opStore.add(expression.charAt(i));
+                num = "";
+            }
         }
+        numStore.add(Long.parseLong(num));
+        System.out.println(numStore);
+        System.out.println(opStore);
+        dfs(0, new char[3]);
         answer = max;
-        System.out.println("정답 : " + answer);
+        System.out.println(answer);
         return answer;
     }
 
-    public static void dfs(String expression, int length) {
+    public static void dfs(int length, char[] ops) {
         if (length == 3) {
-            System.out.println("최종 op : " + op);
-            sumStore.add(Math.abs(calc(expression)));
-            System.out.println("sumStore : " + sumStore);
-
-        } else {
-            for (int i=0;i<opStore.size();i++) {
-                    if (!op.contains(String.valueOf(opStore.get(i)))) {
-//                        op = op.replace(op.charAt(length), opStore.get(i));
-                        char[] operation = op.toCharArray();
-                        operation[length] = opStore.get(i);
-                        op = String.valueOf(operation[0]) + String.valueOf(operation[1]) + String.valueOf(operation[2]);
-                        System.out.println("bfs 전 op : " + op);
-                        length++;
-                        dfs(expression, length);
-                        op = op.replace(op.charAt(length-1), '0');
-                        System.out.println("종료 후 op : " + op);
-                        length--;
+            List<Long> tmpNumStore = new ArrayList<>(numStore);
+            List<Character> tmpOpStore = new ArrayList<>(opStore);
+//            for (char op : ops) {
+//                System.out.print(op+" ");
+//            }
+            System.out.println();
+            for(int i=0; i<ops.length;i++) {
+                for(int j=0;j<tmpOpStore.size();j++) {
+                    if (ops[i] == tmpOpStore.get(j)) {
+//                        System.out.println("j : " + j);
+//                        System.out.println("ops : " + ops[i] );
+//                        System.out.println("tmpOpStoreSize : " + tmpOpStore.size());
+                        long sum = calc(tmpNumStore.remove(j), tmpNumStore.remove(j), tmpOpStore.remove(j));
+//                        char sumOp = sum > 0 ? '+' : '-';
+                        System.out.println("sum : " + sum);
+//                        System.out.println("sumOp : " + sumOp);
+                        tmpNumStore.add(j, sum);
+//                        tmpOpStore.add(sumOp);
+                        System.out.println("tmpNumStore : "+ tmpNumStore);
+                        System.out.println("tmpOpStore : "+tmpOpStore);
+                        j--;
                     }
                 }
+//                if (tmpNumStore.size() == 1 && tmpOpStore.size() == 1) break;
+            }
+            max = Math.max(max, Math.abs(tmpNumStore.get(0)));
+            System.out.println("max : " + max);
+//            return;
         }
-    }
-
-    public static long calc(String expression) {
-        boolean minus1 = false;
-        boolean minus2 = false;
-        long result = 0;
-        long sum = 0;
-        int position = 0;
-        char pick_word = 0;
-        int index = 0;
-        char[] operation = op.toCharArray();
-        System.out.println("op : " + op);
-        for(int i=0;i<operation.length;i++) {
-            Stack<Integer> opPositionStore = new Stack<>();
-            while (true) {
-                Stack<Character> tmp1 = new Stack<>();
-                List<Character> tmp2 = new ArrayList<>();
-                String str_num1 = "";
-                String str_num2 = "";
-                minus2 = false;
-                minus1 = false;
-//                System.out.println("[" + i + "] expression : " + expression);
-                if (expression.contains(String.valueOf(operation[i]))) {
-                    position = opPositionStore.contains(position) ? expression.indexOf(operation[i], opPositionStore.peek()+1) : expression.indexOf(operation[i]);
-                    if (position == -1) break;
-                    opPositionStore.push(position);
-                    System.out.println("opPositionStore : " + opPositionStore);
-//                    System.out.println("operation : " + operation[i]);
-//                    System.out.println("position : " + position);
-
-//                    if (position == 5) {
-//                        System.out.println(expression.indexOf(position-1));
-//                        System.out.println(expression.charAt(position-1) == '+');
-//                        System.out.println(expression.charAt(position-1) == '*');
-//                    }
-                    if(expression.length() > position && position > 0 && (expression.charAt(position-1) == '+' || expression.charAt(position-1) == '*') && expression.indexOf('-', position+1) == -1) {
-                        System.out.println("접근");
-                        break;
-                    }
-
-                    System.out.println("position : " + position);
-
-                    for (int j = position - 1; j >= 0; j--) {
-                        pick_word = expression.charAt(j);
-//                        System.out.println("pick_word : " + pick_word);
-                        if (operation[i] == '-' && pick_word == '-') {
-                            minus1 = true;
-                            continue;
-                        } else if (operation[i] == '+' && pick_word == '-' && j == 0) {
-                            minus1 = true;
-                            break;
-                        }
-
-                        if (pick_word != '+' && pick_word != '-' && pick_word != '*') {
-                            tmp1.push(pick_word);
-                        } else {
-                            break;
-                        }
-                    }
-                    for (int j = position + 1; j < expression.length(); j++) {
-                        pick_word = expression.charAt(j);
-//                        System.out.println("pick_word : " + pick_word);
-                        if (j == position + 1 && pick_word == '-') {
-                            minus2 = true;
-                            continue;
-                        }
-
-                        if (pick_word != '+' && pick_word != '-' && pick_word != '*') {
-                            tmp2.add(pick_word);
-                        } else {
-                            break;
-                        }
-                    }
-
-//                    System.out.println(tmp1);
-                    if (tmp1.isEmpty()) continue;
-
-                    while (!tmp1.isEmpty()) {
-                        str_num1 += tmp1.pop();
-                    }
-
-                    while (!tmp2.isEmpty()) {
-                        str_num2 += tmp2.get(index);
-                        tmp2.remove(index);
-                    }
-
-
-
-                    System.out.println("str_num1 : " + str_num1);
-                    System.out.println("str_num2 : " + str_num2);
-                    System.out.println("operation : " + operation[i]);
-                    str_num1 = minus1 ? "-" + str_num1 : str_num1;
-                    str_num2 = minus2 ? "-" + str_num2 : str_num2;
-                    System.out.println("str_num1 minus : " +str_num1);
-                    System.out.println("str_num2 minus : " +str_num2);
-                    result = operator(str_num1, str_num2, operation[i]);
-                    System.out.println(result);
-                    expression = expression.replace(str_num1 + operation[i] + str_num2, String.valueOf(result));
-                    expression = expression.replaceAll("--","");
-                    opPositionStore.clear();
-                    System.out.println("expression : " + expression);
-                } else {
-                    break;
-                }
-
-                if (!(expression.contains("+") || expression.contains("-") || expression.contains("*"))) {
-                    sum = Long.parseLong(expression);
-                } else if (expression.charAt(0) == '-' && expression.charAt(1) == '-') {
-                    sum = Long.parseLong(expression.substring(2));
-                } else if (expression.charAt(0) == '-' && (expression.indexOf('+') == -1 && expression.indexOf('*') == -1 && expression.indexOf('-', 1) == -1)) {
-                    sum = Long.parseLong(expression);
+            for(int i=0;i<ops.length;i++) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    ops[length] = defaultOp.get(i);
+                    System.out.println("첫 진입 ops : " + Arrays.toString(ops));
+                    dfs(length + 1, ops);
+                    visited[i] = false;
                 }
             }
-        }
-        System.out.println("sum : " + sum);
-        return sum;
     }
 
-    public static long operator(String n1, String n2, char ops) {
-        long sum = 0;
-        switch (ops) {
-            case '+' : sum = Long.parseLong(n1) + Long.parseLong(n2); break;
-            case '-' : sum = Long.parseLong(n1) - Long.parseLong(n2); break;
-            case '*' : sum = Long.parseLong(n1) * Long.parseLong(n2); break;
+    public static long calc(long num1, long num2, char op) {
+        System.out.println("num1 : " + num1);
+        System.out.println("num2 : " + num2);
+        System.out.println("op : " + op);
+        switch (op) {
+            case '+' : return num1+num2;
+            case '-' : return num1-num2;
+            case '*' : return num1*num2;
             default: break;
         }
-        return sum;
+        return 0;
     }
 }
+
+
