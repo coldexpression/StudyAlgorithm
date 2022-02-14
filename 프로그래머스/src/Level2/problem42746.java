ba16d066ff519a6d55073e8145a1621a40a71532
coldexpression <uni_vers_@naver.com>
2022-02-15 01:27:18 +0900

    [정답 - 재풀이(Comparator 이용)] 42746번

--- 프로그래머스/src/Level2/problem42746.java
+++ 프로그래머스/src/Level2/problem42746.java
@@ -1,9 +1,6 @@
 package Level2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 public class problem42746 {
 
@@ -19,81 +16,28 @@ public class problem42746 {
     }
 
     public String solution(int[] numbers) {
-        Stack<String> tmpStore = new Stack<>();
-        String answer = "";
-        char pivotWord = 0;
-        int maxLength = 0;
-        int count = 0;
-        int startIndex = 0;
-        int pivotLength = 0;
-        String[] number = new String[numbers.length];
-        for(int i=0;i<number.length;i++) number[i] = String.valueOf(numbers[i]);
-        Arrays.sort(number);
-        for(int i=number.length-1;i>=0;i--) {
-            board = "";
-            max = -1;
-            wordCount = 0;
-            if (pivotWord != number[i].charAt(0)) {
-                pivotWord = number[i].charAt(0);
-                maxLength = number[i].length();
-                System.out.println("i : " + i);
-                System.out.println("pivotWord :" + pivotWord);
-                for (int j = i - 1; j >= 0; j--) {
-                    if (number[j].charAt(0) == pivotWord) {
-                        count++;
-                    } else {
-                        break;
-                    }
-                }
-                startIndex = i - count;
-                if (count == 0) {
-                    answer = answer.concat(number[i]);
-                } else {
-                    System.out.println("startIndex : " + startIndex);
-                    for (int j = 0; j <= 4; j++) {
-                        pivotLength = j;
-                        for (int k = startIndex; k <= i; k++) {
-                            if (number[k].length() == pivotLength) {
-                                store.add(number[k]);
-                            }
-                        }
-                    }
-                    visited = new boolean[count+1];
-                    wordCount = store.size();
-                    count = 0;
-                    System.out.println("store : " + store);
-                    System.out.println("wordCount : " + wordCount);
-                    dfs(0);
-                }
+        String answer ="";
+        Integer[] array = new Integer[numbers.length];
+
+        for(int i=0;i<numbers.length;i++) array[i] = numbers[i];
+
+        Arrays.sort(array, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                String word1 = o1.toString();
+                String word2 = o2.toString();
+                return ((word2+word1)).compareTo(word1+word2);
             }
+        });
+
+        for(int i=0;i<array.length;i++) {
+            answer = answer.concat(String.valueOf(array[i]));
         }
-        System.out.println("max : " + max);
-        answer = max != -1 ? answer.concat(String.valueOf(max)) : answer;
-//        for(String num: number) System.out.println(num);
-//        visited = new boolean[numbers.length];
-//        count = numbers.length;
-//        board = "";
-//        max = -1;
-//        dfs(0, number);
-//        answer = String.valueOf(max);
+
+        if (answer.charAt(0) == '0') answer = "0";
         System.out.println(answer);
         return answer;
     }
 
-    public static void dfs(int start) {
-        if (start == wordCount) {
-            max = Math.max(max, Integer.parseInt(board));
-            System.out.println("board : " + board);
-        }
-            for(int i=0;i<store.size();i++) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    board = board.concat(store.get(i));
-                    dfs(start+1);
-                    board = board.substring(0, board.length()-store.get(i).length());
-                    visited[i] = false;
-                }
-            }
 
-    }
 }
