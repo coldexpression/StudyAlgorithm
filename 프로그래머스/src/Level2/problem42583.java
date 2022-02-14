670ecd8fb229a3c82b952f2b2adee52b64da7b25
coldexpression <uni_vers_@naver.com>
2022-02-14 17:22:18 +0900

    [정답 - 다시 풀어보기] 42583번

--- 프로그래머스/src/Level2/problem42583.java
+++ 프로그래머스/src/Level2/problem42583.java
@@ -1,72 +1,62 @@
 package Level2;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class problem42583 {
 
     public static void main(String[] args) {
         problem42583 problem42583 = new problem42583();
+        Queue<Integer> ridingTrucks = new LinkedList<>();
+        int currentWeight = 0;
+//        ridingTrucks.add(1);
+//        ridingTrucks.add(2);
+//        ridingTrucks.add(3);
+//        ridingTrucks.add(4);
+//        for(int i=0;i<ridingTrucks.size();i++) {
+//            currentWeight += ridingTrucks.element();
+//            System.out.println(ridingTrucks.element());
+//        }
+//        System.out.println(currentWeight);
+//        System.out.println(ridingTrucks.element());
 //        problem42583.solution(100,100, new int[]{10,10,10,10,10,10,10,10,10,10});
-//        problem42583.solution(5,5,new int[]{1,1,1,1,1,2,2});
+        problem42583.solution(2,10,new int[]{7,4,5,6});
 //        problem42583.solution(100,100,new int[]{10});
     }
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 1;
-        Stack<Integer> waitList = new Stack<>();
-        Queue<Integer> ridingTrucks = new LinkedList<>();
+        Stack<Integer> waitTrucks = new Stack<>();
+        Queue<Integer> ridingTrucks = new ArrayDeque<>();
+        Queue<Integer> waitingTrucks = new LinkedList<>();
         int currentWeight = 0;
-        int count = 0;
         int time = 1;
-        int firstTruckWeight = 0;
-        int ridingWeight = 0;
-        for (int i=truck_weights.length-1;i>=0;i--) {
-            waitList.push(truck_weights[i]);
+        int index = 0;
+
+
+        for (int i=0;i<bridge_length - 1;i++) {
+            ridingTrucks.add(0);
         }
 
-        System.out.println(waitList);
+        currentWeight = truck_weights[0];
+        ridingTrucks.add(currentWeight);
+        index = 1;
 
-        while(!waitList.isEmpty()) {
-            count = 0;
-            currentWeight = waitList.pop();
-            firstTruckWeight = currentWeight;
-            count++;
-            while(true) {
-                if (waitList.isEmpty()) break;
-                if (currentWeight + waitList.peek() <= weight) {
-                    ridingTrucks.add(waitList.peek());
-                    currentWeight += waitList.pop();
-                    count++;
-                } else break;
-            }
+        while(!ridingTrucks.isEmpty()) {
+            time++;
 
-            System.out.println("currentWeight : " + currentWeight);
-            System.out.println("count : " + count);
-            System.out.println("firstTruckWeight : " + firstTruckWeight);
-            time += bridge_length;
-            ridingWeight = currentWeight;
-            for(int i=1;i<count;i++) {
-               time += 1;
-            }
+            int truck = ridingTrucks.poll();
 
-            if (!waitList.isEmpty() && ridingTrucks.size() > 1) {
-                for (int i = 1; i <= ridingTrucks.size(); i++) {
-                    ridingWeight -= ridingTrucks.peek();
-                    if (ridingWeight + waitList.peek() <= weight) {
-                        time -= i;
-                        break;
-                    }
+            currentWeight -= truck;
+
+            if (index < truck_weights.length) {
+                if (currentWeight + truck_weights[index] <= weight) {
+                    currentWeight += truck_weights[index];
+                    ridingTrucks.add(truck_weights[index++]);
+                } else {
+                    ridingTrucks.add(0);
                 }
             }
-
-//            if (!waitList.isEmpty()) {
-//                if (currentWeight - firstTruckWeight > 0 && currentWeight - firstTruckWeight + waitList.peek() <= weight) time -= 1;
-//            }
-            System.out.println("time : " + time);
         }
-
         answer = time;
         System.out.println(answer);
 
