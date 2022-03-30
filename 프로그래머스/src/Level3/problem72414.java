9ad4840463f59587069b4a809f6207a4d0e607f2
coldexpression <uni_vers_@naver.com>
2022-03-30 10:52:12 +0900

    [풀이 중] 72414번

--- 프로그래머스/src/Level3/problem72414.java
+++ 프로그래머스/src/Level3/problem72414.java
@@ -6,110 +6,77 @@ public class problem72414 {
 
     public static void main(String[] args) {
         problem72414 problem72414 = new problem72414();
-//        problem72414.solution("02:03:55","00:14:15",new String[]{"01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"});
+        problem72414.solution("02:03:55","00:14:15",new String[]{"01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"});
 //        problem72414.solution("99:59:59","25:00:00",new String[]{"69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"});
-        problem72414.solution("02:03:55","00:14:15",new String[]{"01:20:15-01:45:14", "00:25:50-00:48:29", "00:40:31-01:00:00", "01:37:44-02:02:30", "01:30:59-01:53:29"});
+//        problem72414.solution("02:03:55","00:14:15",new String[]{"01:20:15-01:45:14", "00:25:50-00:48:29", "00:40:31-01:00:00", "01:37:44-02:02:30", "01:30:59-01:53:29"});
     }
 
     public String solution(String play_time, String adv_time, String[] logs) {
         String answer = "";
-        HashMap<Integer, long[]> logStore = new HashMap<>();
-        HashMap<Integer, Long> answerStore = new HashMap<>();
-        long playTime = stringToLong(play_time);
+        HashMap<Long, Long> resultMap = new HashMap<>();
+        List<String> logList = new ArrayList<>();
+        long playEndTime = stringToLong(play_time);
         long advTime = stringToLong(adv_time);
-        int answerTime = Integer.MAX_VALUE;
-        int resultCount = Integer.MIN_VALUE;
-        int count = 1;
-        if (playTime == advTime) return "00:00:00";
-        for(int i=0;i<logs.length;i++) {
-            String start = logs[i].split("-")[0];
-            String end = logs[i].split("-")[1];
-            long startTime = stringToLong(start);
-            long endTime = stringToLong(end);
-            logStore.put(i, new long[]{startTime, endTime});
-        }
+        Arrays.sort(logs, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                long t1 = stringToLong(o1.split("-")[0]) + stringToLong(o1.split("-")[1]);
+                long t2 = stringToLong(o2.split("-")[0]) + stringToLong(o2.split("-")[1]);
+                return (int)t1-(int)t2;
+            }
+        });
+        logList.addAll(Arrays.asList(logs));
 
-        for (Integer integer : logStore.keySet()) {
-            long[] temp = logStore.get(integer);
-            System.out.println(integer + " 번째");
-            System.out.println(longToString(temp[0]));
-            System.out.println(longToString(temp[1]));
-        }
+        for(long i=0;advTime+i<=playEndTime;i++) {
+            long start = i;
+            long end = advTime + start;
+            long totalPlayTime = 0;
+            List<String> currentList = new ArrayList<>();
+            for(int j=0;j<logList.size();j++) {
+                if (logList.isEmpty()) break;
+                long logStart = stringToLong(logList.get(j).split("-")[0]);
+                long logEnd = stringToLong(logList.get(j).split("-")[1]);
+                if ((start <= logStart && end >= logEnd) || (start > logStart && end < logEnd) || (start <= logStart && end >= logStart) || (start <= logEnd && end >= logEnd)) {
+                    currentList.add(logList.get(j));
+                } else if (start > logEnd) {
+                    logList.remove(j);
+                    j = j - 1;
+                } else if (logStart > start && logStart > end) break;
+            }
 
-        // 시작 점부터 탐색
-        for(int i=0;i<logs.length;i++) {
-            long[] pivotTime = logStore.get(i);
-            long startTime = pivotTime[0];
-            long endTime = pivotTime[0] + advTime;
-            System.out.println("pivotTime : " + longToString(pivotTime[0]));
-            count = 1;
-            if (endTime <= playTime) {
-                for(int j=0;j<logs.length;j++) {
-                    if (i != j) {
-                        long[] compareTime = logStore.get(j);
-                        System.out.println("["+longToString(compareTime[0])+", "+longToString(compareTime[1])+"]");
-                        if ((compareTime[0] >= startTime && compareTime[0] <= endTime) || (compareTime[1] >= startTime && compareTime[1] <= endTime)
-                                || (compareTime[0] <= startTime && compareTime[1] >= startTime)) {
-                            count++;
-                            System.out.println(j+" 번째는 통과");
-                        }
+            if (!currentList.isEmpty()) {
+                System.out.println(currentList);
+//                start = Math.max(start, stringToLong(currentList.get(0).split("-")[0]));
+                System.out.println("start : " + longToString(start));
+                System.out.println("end : " + longToString(end));
+                if (currentList.size() > 1) {
+                    for (int j = 0; j < currentList.size(); j++) {
+                        long t1Start = stringToLong(currentList.get(j).split("-")[0]);
+                        t1Start = Math.max(t1Start, start);
+                        long t1End = stringToLong(currentList.get(j).split("-")[1]);
+                        t1End = Math.min(t1End, end);
+                        totalPlayTime += t1End - t1Start;
                     }
+                } else if (currentList.size() == 1){
+                    long t1Start = stringToLong(currentList.get(0).split("-")[0]);
+                    long t1End = stringToLong(currentList.get(0).split("-")[1]);
+                    t1End = Math.min(t1End, end);
+                    totalPlayTime += t1End - t1Start;
                 }
-                if (resultCount <= count) {
-                    answerStore.put(count, Math.min(answerStore.getOrDefault(count, Long.MAX_VALUE), startTime));
-                    resultCount = count;
-                }
+                if (totalPlayTime > 0)
+                    resultMap.put(totalPlayTime, Math.min(resultMap.getOrDefault(totalPlayTime, Long.MAX_VALUE), start));
+            } else {
+                i = stringToLong(logList.get(0).split("-")[0])-advTime-1;
             }
         }
-
-        // 종료 점부터 탐색
-        for(int i=0;i<logs.length;i++) {
-            long[] pivotTime = logStore.get(i);
-            long startTime = pivotTime[1];
-            long endTime = pivotTime[1] + advTime;
-            System.out.println("pivotTime : " + longToString(pivotTime[1]));
-            count = 1;
-            if (endTime <= playTime) {
-                for (int j=0;j<logs.length;j++) {
-                    if (i != j) {
-                        long[] compareTime = logStore.get(j);
-                        System.out.println("["+longToString(compareTime[0])+", "+longToString(compareTime[1])+"]");
-                        if ((compareTime[0] >= startTime && compareTime[0] <= endTime) || (compareTime[1] >= startTime && compareTime[1] <= endTime)
-                                || (compareTime[0] <= startTime && compareTime[1] >= startTime)) {
-                            count++;
-                            System.out.println(j+" 번째는 통과");
-                        }
-                    }
-                }
-                if (resultCount <= count) {
-                    answerStore.put(count, Math.min(answerStore.getOrDefault(count, Long.MAX_VALUE), startTime));
-                    resultCount = count;
-                }
-            }
-        }
-
-//        for(int i=0;i<logs.length;i++) {
-//            count = 1;
-//            int[] pivotTime = logStore.get(i);
-//            System.out.println("pivotTime : " + intToString(pivotTime[0]));
-//            for(int j=0;j<logs.length;j++) {
-//                if(i != j) {
-//                    if(pivotTime[0] + advTime >= logStore.get(j)[0] && pivotTime[0] <= logStore.get(j)[1]) {
-//                        System.out.println(j+" 번째는 통과");
-//                        count++;
-//                    }
-//                }
-//            }
-//            if(resultCount <= count) {
-////                System.out.println("pivotTime : " + intToString(pivotTime[0]));
-//                System.out.println("count : " + count);
-//                answerStore.put(count, Math.min(answerStore.getOrDefault(count, Integer.MAX_VALUE), pivotTime[0]));
-//                resultCount = count;
-//            }
-//        }
-        System.out.println(answerStore);
-        answer = longToString(answerStore.get(resultCount));
-        System.out.println("정답 " + answer);
+        System.out.println(resultMap);
+//        System.out.println(longToString(3704));
+//        System.out.println(longToString(696));
+        long max = 0;
+        for(long key: resultMap.keySet()) max = Math.max(max, key);
+        System.out.println(longToString(resultMap.get(max)));
+        System.out.println(longToString(max));
+        answer = longToString(resultMap.get(max));
         return answer;
     }
 
