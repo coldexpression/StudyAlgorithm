11ba55cf23109b01446055ae4a77e7e525f2d379
coldexpression <uni_vers_@naver.com>
2022-04-01 00:02:29 +0900

    [정답 - DP이용] 42898번

--- 프로그래머스/src/Level3/problem42898.java
+++ 프로그래머스/src/Level3/problem42898.java
@@ -4,67 +4,60 @@ import java.util.*;
 
 public class problem42898 {
 
-    static boolean[][] visited;
-    static int[][] board;
-    static int answer;
-
     public static void main(String[] args) {
         problem42898 problem42898 = new problem42898();
         problem42898.solution(4, 3, new int[][]{{2,2}});
     }
 
     public int solution(int m, int n, int[][] puddles) {
-        answer = 0;
-        int min = 0;
-        board = new int[n][m];
-        visited = new boolean[n][m];
-        for(int i=0;i<visited.length;i++)
-            Arrays.fill(visited[i], false);
-
+        int answer = 0;
+        int[][] dp = new int[n][m];
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+                dp[i][j] = -1;
+        for(int i=0;i<n;i++) dp[i][0] = 1;
+        for(int i=0;i<m;i++) dp[0][i] = 1;
+        System.out.println("최대 X : " + m + " 최대 Y : " + n);
         for(int i=0;i<puddles.length;i++) {
             int col = puddles[i][0] - 1;
             int row = puddles[i][1] - 1;
-            board[row][col] = -1;
-            visited[row][col] = true;
+            System.out.println("col : " + col + " row : " + row);
+            if (row == 0) {
+                for(int j=col;j<m;j++) dp[row][j] = 0;
+            } else if (col == 0) {
+                for(int j=row;j<n;j++) dp[j][col] = 0;
+            } else dp[row][col] = 0;
         }
 
-        board[0][0] = 0;
-        dfs(0, 0, n-1, m-1);
-        System.out.println(answer);
+        if (m == 1) {
+            for(int i=0;i<n;i++) {
+                if (dp[i][0] == 0) return 0;
+                else answer = 1;
+            }
+        } else if (n == 1) {
+            for(int i=0;i<m;i++) {
+                if (dp[0][i] == 0) return 0;
+                else answer = 1;
+            }
+        } else {
+            for (int i = 1; i < n; i++) {
+                for (int j = 1; j < m; j++) {
+                    if (dp[i][j] != 0) {
+                        dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
+                    }
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++) {
+            System.out.println();
+            for (int j = 0; j < m; j++) {
+                System.out.print("["+dp[i][j]+"]");
+            }
+        }
+        System.out.println();
+        System.out.println(dp[n-1][m-1]);
         return answer;
     }
 
-    private void dfs(int startRow, int startCol, int endRow, int endCol) {
-        if (startRow == endRow && startCol == endCol) {
-            answer = board[startRow][startCol];
-            return ;
-        }
-
-        if (!visited[startRow][startCol]) {
-//            System.out.println("["+startCol+","+startRow+"]");
-            visited[startRow][startCol] = true;
-            int prev;
-            if (startRow + 1 <= endRow && startCol + 1 > endCol) {
-                prev = board[startRow][startCol];
-                board[startRow+1][startCol] = (board[startRow][startCol] + 1) % 1000000007;
-                dfs(startRow+1, startCol, endRow, endCol);
-                board[startRow+1][startCol] = prev;
-            } else if (startCol + 1 <= endCol && startRow + 1 > endRow) {
-                prev = board[startRow][startCol+1];
-                board[startRow][startCol+1] = (board[startRow][startCol] + 1) % 1000000007;
-                dfs(startRow, startCol + 1, endRow, endCol);
-                board[startRow][startCol+1] = prev;
-            } else if (startRow + 1 <= endRow && startCol + 1 <= endCol) {
-                prev = board[startRow][startCol];
-                board[startRow+1][startCol] = (board[startRow][startCol] + 1) % 1000000007;
-                dfs(startRow+1, startCol, endRow, endCol);
-                board[startRow+1][startCol] = prev;
-                board[startRow][startCol+1] = (board[startRow][startCol] + 1) % 1000000007;
-                dfs(startRow, startCol + 1, endRow, endCol);
-                board[startRow][startCol+1] = prev;
-            }
-            visited[startRow][startCol] = false;
-        }
-
-    }
 }
