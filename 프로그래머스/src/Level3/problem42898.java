0f1c5d41f541412d1c122b2e2bb5e64465824f41
coldexpression <uni_vers_@naver.com>
2022-03-31 17:29:27 +0900

    [풀이 중 - DFS이용 시간초과] 42898번

--- 프로그래머스/src/Level3/problem42898.java
+++ 프로그래머스/src/Level3/problem42898.java
@@ -4,93 +4,67 @@ import java.util.*;
 
 public class problem42898 {
 
+    static boolean[][] visited;
+    static int[][] board;
+    static int answer;
+
     public static void main(String[] args) {
         problem42898 problem42898 = new problem42898();
-        problem42898.solution(1, 3, new int[][]{{1,2}});
+        problem42898.solution(4, 3, new int[][]{{2,2}});
     }
 
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        int[][] board = new int[n][m];
-        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>();
-        PriorityQueue<Integer> answerStore;
-        for (int i = 0; i < puddles.length; i++) {
-            int x = puddles[i][0] - 1;
-            int y = puddles[i][1] - 1;
-            board[y][x] = -1;
-        }
-        for (int i = 0; i < board[0].length; i++) {
-            if (board[0][i] == -1) break;
-            PriorityQueue<Integer> queue = new PriorityQueue<>();
-            queue.add(i + 1);
-            map.put(i, queue);
+        answer = 0;
+        int min = 0;
+        board = new int[n][m];
+        visited = new boolean[n][m];
+        for(int i=0;i<visited.length;i++)
+            Arrays.fill(visited[i], false);
+
+        for(int i=0;i<puddles.length;i++) {
+            int col = puddles[i][0] - 1;
+            int row = puddles[i][1] - 1;
+            board[row][col] = -1;
+            visited[row][col] = true;
         }
 
-        if (m == 1) {
-            for(int i=0;i<n;i++) if (board[i][0] == -1) return 0;
-        } else if (n == 1) {
-            for(int i=0;i<m;i++) if (board[0][i] == -1) return 0;
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.println("[" + i + "," + j + "]");
-                if (board[i][j] != -1) {
-                    PriorityQueue<Integer> queue;
-                    PriorityQueue<Integer> newQueue;
-                    PriorityQueue<Integer> prevQueue;
-                    if (j == 0 && board[i - 1][j] != -1) {
-                        queue = map.getOrDefault(j, new PriorityQueue<>());
-                        newQueue = new PriorityQueue<>();
-                        while (!queue.isEmpty()) {
-                            newQueue.add(queue.poll() + 1);
-                        }
-                        map.put(j, newQueue);
-                    }  else {
-                        if (board[i][j - 1] == -1 && board[i - 1][j] != -1) {
-                            queue = map.getOrDefault(j, new PriorityQueue<>());
-                            newQueue = new PriorityQueue<>();
-                            while (!queue.isEmpty()) {
-                                newQueue.add(queue.poll() + 1);
-                            }
-                            map.put(j, newQueue);
-                        } else if (board[i][j - 1] != -1 && board[i - 1][j] == -1) {
-                            queue = map.getOrDefault(j - 1, new PriorityQueue<>());
-                            prevQueue = new PriorityQueue<>(queue);
-                            newQueue = new PriorityQueue<>();
-                            while (!queue.isEmpty()) {
-                                newQueue.add(queue.poll() + 1);
-                            }
-                            map.put(j - 1, prevQueue);
-                            map.put(j, newQueue);
-                        } else if (board[i][j - 1] != -1 && board[i - 1][j] != -1) {
-                            Queue<Integer> queue1 = map.getOrDefault(j, new PriorityQueue<>());
-                            Queue<Integer> queue2 = map.getOrDefault(j - 1, new PriorityQueue<>());
-                            prevQueue = new PriorityQueue<>(queue2);
-                            newQueue = new PriorityQueue<>();
-                            while (!queue1.isEmpty()) newQueue.add(queue1.poll() + 1);
-                            while (!queue2.isEmpty()) newQueue.add(queue2.poll() + 1);
-                            map.put(j, newQueue);
-                            map.put(j - 1, prevQueue);
-                        }
-                    }
-                }
-                System.out.println(map);
-            }
-        }
-        System.out.println(map.get(m - 1));
-        answerStore = map.getOrDefault(m - 1, new PriorityQueue<>());
-        if (answerStore.isEmpty()) return 0;
-        int min = answerStore.peek();
-        while (!answerStore.isEmpty()) {
-            if (min != answerStore.peek()) break;
-            answer = (answer % 1000000007) + 1;
-            answerStore.poll();
-        }
-
+        board[0][0] = 0;
+        dfs(0, 0, n-1, m-1);
         System.out.println(answer);
-
-
         return answer;
+    }
+
+    private void dfs(int startRow, int startCol, int endRow, int endCol) {
+        if (startRow == endRow && startCol == endCol) {
+            answer = board[startRow][startCol];
+            return ;
+        }
+
+        if (!visited[startRow][startCol]) {
+//            System.out.println("["+startCol+","+startRow+"]");
+            visited[startRow][startCol] = true;
+            int prev;
+            if (startRow + 1 <= endRow && startCol + 1 > endCol) {
+                prev = board[startRow][startCol];
+                board[startRow+1][startCol] = (board[startRow][startCol] + 1) % 1000000007;
+                dfs(startRow+1, startCol, endRow, endCol);
+                board[startRow+1][startCol] = prev;
+            } else if (startCol + 1 <= endCol && startRow + 1 > endRow) {
+                prev = board[startRow][startCol+1];
+                board[startRow][startCol+1] = (board[startRow][startCol] + 1) % 1000000007;
+                dfs(startRow, startCol + 1, endRow, endCol);
+                board[startRow][startCol+1] = prev;
+            } else if (startRow + 1 <= endRow && startCol + 1 <= endCol) {
+                prev = board[startRow][startCol];
+                board[startRow+1][startCol] = (board[startRow][startCol] + 1) % 1000000007;
+                dfs(startRow+1, startCol, endRow, endCol);
+                board[startRow+1][startCol] = prev;
+                board[startRow][startCol+1] = (board[startRow][startCol] + 1) % 1000000007;
+                dfs(startRow, startCol + 1, endRow, endCol);
+                board[startRow][startCol+1] = prev;
+            }
+            visited[startRow][startCol] = false;
+        }
+
     }
 }
