629656a667b8a26b29cdf5e702d006517ec5f629
coldexpression <uni_vers_@naver.com>
2022-04-20 00:02:03 +0900

    임시 메소드 필기

---
+++ 프로그래머스/src/bucketplaces/p1.java
@@ -0,0 +1,18 @@
+package bucketplaces;
+
+import java.util.*;
+
+public class p1 {
+
+    public static void main(String[] args) {
+        HashMap<Integer, List<String>> store = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        Iterator<String> iterator = set.iterator();
+        List<String> list = new ArrayList<>();
+        String[] s = list.stream().toArray();
+        String[] str;
+        str.length;
+        String word = "";
+        word.toLowerCase();
+    }
+}
