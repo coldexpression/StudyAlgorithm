f91df02d1aa28ade68678513a0b0c55e6faa49bc
coldexpression <uni_vers_@naver.com>
2022-05-18 12:25:57 +0900

    [정답 - 재풀이 필요] 92344번 - 누적합 이용

--- 프로그래머스/src/Level3/problem92344.java
+++ 프로그래머스/src/Level3/problem92344.java
@@ -13,122 +13,41 @@ public class problem92344 {
 
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
-        int m = board.length;
-        int n = board[0].length;
-        int index = 0;
-        int start = -1;
-        int end = -1;
-        int degree = 0;
-        int count = 0;
-        int[] map = new int[m*n];
-        PriorityQueue<startNode> startQueue = new PriorityQueue<>(new Comparator<startNode>() {
-            @Override
-            public int compare(startNode o1, startNode o2) {
-                return o1.getStartIndex() - o2.getStartIndex();
-            }
-        });
-        PriorityQueue<endNode> endQueue = new PriorityQueue<>(new Comparator<endNode>() {
-            @Override
-            public int compare(endNode o1, endNode o2) {
-                return o1.getEndIndex() - o2.getEndIndex();
-            }
-        });
-
-        for(int i=0;i<m;i++) {
-            for (int j = 0; j < n; j++) {
-                map[index++] = board[i][j];
-            }
-        }
-        index = 0;
+        int[][] map = new int[board.length+1][board[0].length+1];
 
         for (int[] ints : skill) {
             int type = ints[0];
-            int value = type == 1 ? ints[5]*(-1) : ints[5];
-            int startIndex = transPosition(ints[1], ints[2], n);
-            int endIndex = transPosition(ints[3], ints[4], n);
-            startNode startNode = new startNode(startIndex, value);
-            endNode endNode = new endNode(endIndex, value*(-1));
-            startQueue.add(startNode);
-            endQueue.add(endNode);
-            System.out.println("조건 startIndex : " + startIndex);
-            System.out.println("조건 endIndex : " + endIndex);
+            int degree = ints[5];
+            int row1 = ints[1];
+            int col1 = ints[2];
+            int row2 = ints[3];
+            int col2 = ints[4];
+            degree = type == 1 ? degree*(-1) : degree;
+            map[row1][col1] += degree;
+            map[row1][col2+1] += degree*(-1);
+            map[row2+1][col1] += degree*(-1);
+            map[row2+1][col2+1] += degree;
         }
 
         for(int i=0;i<map.length;i++) {
-            if (!startQueue.isEmpty()) {
-                start = startQueue.peek().getStartIndex();
+            for(int j=1;j<map[i].length;j++) {
+                map[i][j] += map[i][j-1];
             }
-            if (!endQueue.isEmpty()) {
-                end = endQueue.peek().getEndIndex();
-            }
-
-            if (start == i) {
-                while(true) {
-                    startNode node = startQueue.poll();
-                    degree += node.getValue();
-                    if (startQueue.isEmpty() || startQueue.peek().getStartIndex() != i) break;
-                }
-            }
-
-            if (end == i - 1) {
-                while(true) {
-                    endNode node = endQueue.poll();
-                    degree += node.getValue();
-                    if (endQueue.isEmpty() || endQueue.peek().getEndIndex() != i - 1) break;
-                }
-            }
-
-            System.out.println("currentIndex : " + i);
-            System.out.println("startIndex : " + start);
-            System.out.println("endIndex : " + end);
-            System.out.println("degree : " + degree);
-
-            map[i] += degree;
-            System.out.println(map[i]);
-            count = map[i] > 0 ? count + 1 : count;
-
         }
 
-        return count;
+        for(int i=0;i<map[0].length;i++) {
+            for(int j=1;j<map.length;j++) {
+                map[j][i] += map[j-1][i];
+            }
+        }
+
+        for(int i=0;i<board.length;i++) {
+            for(int j=0;j<board[i].length;j++) {
+                answer = map[i][j] + board[i][j] > 0 ? answer + 1 : answer;
+            }
+        }
+
+        return answer;
     }
 
-    public int transPosition(int startRow, int startCol, int n) {
-        return (startRow*n) + startCol;
-    }
-
-    public class startNode {
-        private int startIndex;
-        private int value;
-
-        public startNode(int startIndex, int value) {
-            this.startIndex = startIndex;
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public int getStartIndex() {
-            return startIndex;
-        }
-    }
-
-    public class endNode {
-        private int endIndex;
-        private int value;
-
-        public endNode(int endIndex, int value) {
-            this.endIndex = endIndex;
-            this.value = value;
-        }
-
-        public int getValue() {
-            return value;
-        }
-
-        public int getEndIndex() {
-            return endIndex;
-        }
-    }
 }
