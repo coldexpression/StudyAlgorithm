1050364701f63b26b091ded2f3e5b93b4a15fb46
coldexpression <uni_vers_@naver.com>
2022-06-02 00:11:19 +0900

    [정답] 12936번 - 재풀이 필요 [DP]

--- 프로그래머스/src/Level3/problem12936.java
+++ 프로그래머스/src/Level3/problem12936.java
@@ -3,55 +3,64 @@ package Level3;
 import java.util.*;
 
 public class problem12936 {
-
-    static boolean[] visited;
-    static List<int[]> store;
-    static long endIndex;
-    static long startIndex;
-    static long[] answer;
     public static void main(String[] args) {
         problem12936 problem12936 = new problem12936();
-        problem12936.solution(3, 3);
+        problem12936.solution(5, 10);
     }
 
     public long[] solution(int n, long k) {
-        List<Long> list = new ArrayList<>();
-        List<Long> dp = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        List<Integer> dp = new ArrayList<>();
         int totalNum = n;
-        int middleNum = 0;
-        long count = 0;
-        answer = new long[n];
-        visited = new boolean[n];
-        store = new ArrayList<>();
-        int start = 0;
-        Arrays.fill(visited, false);
+        int index = 0;
+        long[] answer = new long[n];
 
-        dp.add(0L);
-        dp.add(1L);
-        dp.add(2L);
+        for (int i = 1; i <= n; i++) list.add(i);
 
-        for(int i=3;i<=n;i++) {
-            dp.add(i*dp.get(i-1));
-        }
-        for(int i=totalNum-1;i>0;i--) totalNum *= i;
-        middleNum = (totalNum / n);
-        k = k - 1;
-        endIndex = k;
+        dp.add(0);
+        dp.add(1);
+        dp.add(2);
 
-        for(int i=middleNum;i<=totalNum;i=i+middleNum) {
-            count++;
-            if (i-middleNum <= k && i-1 >= k) {
-                startIndex = i-middleNum;
-                start = i - middleNum;
+        for (int i = 3; i <= n; i++) dp.add(i * dp.get(i - 1));
+
+        System.out.println(dp);
+        System.out.println(list);
+
+
+        while (index != totalNum) {
+            int divNum = dp.get(n) / n;
+            int mok = (int) (k / divNum);
+            int rest = (int) (k % divNum);
+            k = rest;
+
+            System.out.println("divNum : " + divNum);
+            System.out.println("mok : " + mok);
+            System.out.println("rest : " + rest);
+
+            if (rest == 0) {
+                mok = mok - 1;
+                answer[index++] = list.get(mok);
+                list.remove(mok);
+                while (list.size() != 0) {
+                    answer[index++] = list.get(list.size() - 1);
+                    list.remove(list.size() - 1);
+                }
                 break;
+            } else if (rest == 1) {
+                answer[index++] = list.get(mok);
+                list.remove(mok);
+                while (list.size() != 0) {
+                    answer[index++] = list.get(0);
+                    list.remove(0);
+                }
+                break;
+            } else {
+                answer[index++] = list.get(mok);
+                list.remove(mok);
             }
+            n--;
         }
 
-        list.add(count);
-
-
-
-        dfs(1, n ,list);
 
         for (long l : answer) {
             System.out.print(l + " ");
@@ -59,28 +68,4 @@ public class problem12936 {
         return answer;
     }
 
-    private void dfs(int start, long end, List<Long> tempList) {
-        if (startIndex > endIndex) return;
-
-        if (start == end) {
-            answer = tempList.stream().mapToLong(i -> i).toArray();
-            startIndex++;
-            return;
-        }
-
-        if (!visited[start]) {
-            List<Long> list = new ArrayList<>(tempList);
-            visited[start]  = true;
-            for (long i = 1; i <= end; i++) {
-                if (!list.contains(i)) {
-                    list.add(i);
-                    System.out.println("전 list >> " + list);
-                    dfs(start + 1, end,list);
-                    list = new ArrayList<>(tempList);
-                    System.out.println("후 list >> " + list);
-                }
-            }
-            visited[start] = false;
-        }
-    }
 }
