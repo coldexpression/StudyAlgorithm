9c9f17a190584061e0e05a70dcd52a5880222195
coldexpression <uni_vers_@naver.com>
2022-06-08 00:04:03 +0900

    [정답] 67259번

--- 프로그래머스/src/Level3/problem67259.java
+++ 프로그래머스/src/Level3/problem67259.java
@@ -4,10 +4,8 @@ import java.util.*;
 
 public class problem67259 {
 
-    static int[][] visited;
     static int[] directRow;
     static int[] directCol;
-    static int price;
 
     public static void main(String[] args) {
         problem67259 problem67259 = new problem67259();
@@ -15,54 +13,78 @@ public class problem67259 {
     }
 
     public int solution(int[][] board) {
-        int answer = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        int answer = Integer.MAX_VALUE;
         int n = board.length;
-        visited = new int[n][n];
-        directRow = new int[]{1, 0, -1, 0};
-        directCol = new int[]{0, 1, 0, -1};
-        price = Integer.MAX_VALUE;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 1) visited[i][j] = 1;
-                else visited[i][j] = 0;
+        int[][] visited = new int[n][n];
+        directRow = new int[]{-1,1,0,0};
+        directCol = new int[]{0,0,-1,1};
+
+        // MODE : N(0) [중립 모드]
+        // MODE : V(-1) [세로 모드]
+        // MODE : H(1) [가로 모드]
+        visited[0][0] = 0;
+        queue.add(new int[]{0,0,0,0});
+
+        while(!queue.isEmpty()) {
+            int[] ele = queue.poll();
+            int mode = ele[2];
+            int initPrice = ele[3];
+            System.out.println("현재 좌표 : [" + ele[0] + ", " + ele[1] + "]");
+            System.out.println("현재 모드 : " + mode);
+            System.out.println("현재 가격 : " + initPrice);
+
+            if (ele[0] == n-1 && ele[1] == n-1) answer = Math.min(answer, initPrice);
+
+            for(int i=0;i<4;i++) {
+                int nextRow = ele[0] + directRow[i];
+                int nextCol = ele[1] + directCol[i];
+                int nextMode = 0;
+                int price = initPrice;
+
+                if (nextRow >= 0 && nextRow < n && nextCol >= 0 && nextCol < n && board[nextRow][nextCol] == 0) {
+                    if (visited[nextRow][nextCol] == 0) {
+                        if (mode == 0) {
+                            nextMode = directRow[i] != 0 ? -1 : 1;
+                            price += 100;
+                        } else if (mode == 1) {
+                            nextMode = directRow[i] != 0 ? -1 : 1;
+                            price = nextMode == -1 ? price + 500 : price;
+                            price += 100;
+                        } else if (mode == -1) {
+                            nextMode = directCol[i] != 0 ? 1 : -1;
+                            price = nextMode == 1 ? price + 500 : price;
+                            price += 100;
+                        }
+                        queue.add(new int[]{nextRow, nextCol, nextMode, price});
+                        visited[nextRow][nextCol] = price;
+                    } else {
+                        if (visited[nextRow][nextCol] > price) {
+                            if (mode == 0) {
+                                nextMode = directRow[i] != 0 ? -1 : 1;
+                                price += 100;
+                            } else if (mode == 1) {
+                                nextMode = directRow[i] != 0 ? -1 : 1;
+                                price = nextMode == -1 ? price + 500 : price;
+                                price += 100;
+                            } else if (mode == -1) {
+                                nextMode = directCol[i] != 0 ? 1 : -1;
+                                price = nextMode == 1 ? price + 500 : price;
+                                price += 100;
+                            }
+                            queue.add(new int[]{nextRow, nextCol, nextMode, price});
+                            visited[nextRow][nextCol] = price;
+                        }
+
+                    }
+                }
             }
+
         }
-        dfs(0, 0, n-1, 'N', 0);
-        answer = price;
+
+        System.out.println(answer);
+
         return answer;
     }
 
-    private void dfs(int startRow, int startCol, int end,  char mode, int sum) {
-        if (startRow == end && startCol == end && sum < price) {
-            price = sum;
-            return;
-        }
-
-        if (sum < price && visited[startRow][startCol] == 0) {
-            visited[startRow][startCol] = 1;
-            for (int i = 0; i < 4; i++) {
-                int nextRow = startRow + directRow[i];
-                int nextCol = startCol + directCol[i];
-                char currentMode = mode;
-                int checkRow , checkCol = 0;
-                int total = sum;
-                if (nextRow >= 0 && nextRow <= end && nextCol >= 0 && nextCol <= end) {
-                    checkRow = nextRow - startRow;
-                    checkCol = nextCol - startCol;
-                    if (mode == 'N') {
-                        currentMode = checkRow != 0 ? 'R' : 'C';
-                    } else if (mode == 'R') {
-                        currentMode = checkCol != 0 ? 'C' : 'R';
-                        total = checkCol != 0 ? total + 500 : total;
-                    } else {
-                        currentMode = checkRow != 0 ? 'R' : 'C';
-                        total = checkRow != 0 ? total + 500 : total;
-                    }
-                    total += 100;
-                    dfs(nextRow, nextCol, end, currentMode, total);
-                }
-            }
-            visited[startRow][startCol] = 0;
-        }
-    }
 }
