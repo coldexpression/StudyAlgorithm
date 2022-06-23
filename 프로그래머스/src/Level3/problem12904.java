404a5c4e05c67f8a1dd1526cdf350c8ca13bc93f
coldexpression <uni_vers_@naver.com>
2022-06-23 21:28:12 +0900

    [풀이 중] 12904번 - 시간 초과

--- 프로그래머스/src/Level3/problem12904.java
+++ 프로그래머스/src/Level3/problem12904.java
@@ -6,69 +6,57 @@ public class problem12904 {
     public static void main(String[] args) {
         problem12904 problem12904 = new problem12904();
 //        problem12904.solution("abcdcba");
-        problem12904.solution("abbb");
-//        problem12904.solution("abba");
+//        problem12904.solution("abbb");
+        problem12904.solution("aabccbab");
     }
 
     public int solution(String s) {
-        int answer = Integer.MIN_VALUE;
-        char[] word = s.toCharArray();
-        char[] forward = s.toCharArray();
-        char[] reverseWord = new char[s.length()];
-        int[][] dp = new int[s.length()+1][s.length()+1];
-        int[][] reverseDp = new int[s.length()+1][s.length()+1];
-        int forwardMax = Integer.MIN_VALUE;
-        int reverseMax = Integer.MIN_VALUE;
+        int answer = 1;
+        int left = 1;
+        int right = s.length()-2;
+        int length = 1;
         int index = 0;
 
-        for(int i=s.length()-1;i>=0;i--) {
-            reverseWord[index++] = s.charAt(i);
-        }
+        if (reverseString(s)) return s.length();
 
-        for (char c : reverseWord) {
-            System.out.print(c);
-        }
-
-        for(int i=0;i<forward.length;i++) {
-            for(int j=0;j<word.length;j++) {
-                if(forward[i] == word[j]) {
-                    dp[i+1][j+1] = Math.max(dp[i+1][j+1], dp[i][j] + 1);
-                } else {
-                    dp[i+1][j+1] = Math.max(dp[i][j+1], dp[i+1][j]);
+        for(int i=1;i<s.length();i++) {
+            char pivot = s.charAt(i);
+            int endIndex = Math.min(left, right);
+            length = 0;
+            String leftWord = String.valueOf(pivot);
+            String rightWord = String.valueOf(pivot);
+            String word = String.valueOf(pivot);
+            for(int j=1;j<=endIndex;j++) {
+                if (answer == s.length()) break;
+                leftWord = s.charAt(i-j) + word;
+                rightWord = word + s.charAt(i+j);
+                word = s.charAt(i-j) + word + s.charAt(i+j);
+                if (reverseString(leftWord)) {
+                    length = leftWord.length();
+                } else if (reverseString(rightWord)) {
+                    length = rightWord.length();
+                } else if (reverseString(word)) {
+                    length = word.length();
                 }
-                forwardMax = Math.max(forwardMax, dp[i+1][j+1]);
             }
+            left++;
+            right--;
+            answer = Math.max(answer ,length);
         }
-
-        for(int i=0;i<forward.length;i++) {
-            for(int j=0;j<reverseWord.length;j++) {
-                if(forward[i] == reverseWord[j]) {
-                    reverseDp[i+1][j+1] = Math.max(reverseDp[i+1][j+1], reverseDp[i][j] + 1);
-                } else {
-                    reverseDp[i+1][j+1] = Math.max(reverseDp[i][j+1], reverseDp[i+1][j]);
-                }
-                reverseMax = Math.max(reverseMax, reverseDp[i+1][j+1]);
-            }
-        }
-
-        for(int i=0;i<dp.length;i++) {
-            System.out.println();
-            for(int j=0;j<dp[i].length;j++) {
-                System.out.print("["+dp[i][j]+"] ");
-            }
-        }
-
-        System.out.println();
-
-        for(int i=0;i<reverseDp.length;i++) {
-            System.out.println();
-            for(int j=0;j<reverseDp[i].length;j++) {
-                System.out.print("["+reverseDp[i][j]+"] ");
-            }
-        }
-
-        answer = Math.min(forwardMax, reverseMax);
-
+        System.out.println(answer);
         return answer;
+    }
+
+    public boolean reverseString(String word) {
+        int index = 0;
+        boolean check = true;
+        for(int i=word.length()-1;i>=0;i--) {
+            if (word.charAt(index++) != word.charAt(i)) {
+                check = false;
+                break;
+            }
+        }
+
+        return check;
     }
 }
