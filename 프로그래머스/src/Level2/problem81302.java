e7dde92089eb83e94916effafd1d1c26e66ff256
coldexpression <uni_vers_@naver.com>
2022-08-21 21:55:04 +0900

    [정답] 81302번

--- 프로그래머스/src/Level2/problem81302.java
+++ 프로그래머스/src/Level2/problem81302.java
@@ -13,113 +13,78 @@ public class problem81302 {
         String[][] place = new String[5][5];
 //        String[][] test = new String[][]{{"POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"}, {"POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"},{"PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"},
 //                {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},{"PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"}};
-        String[][] test = new String[][]{{"XPXPX", "PXPXP", "XPXPX", "PXPXP", "XPXPX"}, {"OOOXX", "OOOXX", "OOOXX", "OXXXO", "OOOXX"},{"OOOXX", "OOOXX", "OOOXX", "OOOXX", "OOOXX"},
+        String[][] test = new String[][]{{"PXXXX", "XPXXX", "XXXPP", "XXXXX", "XXXXX"}, {"OOOXX", "OOOXX", "OOOXX", "OXXXO", "OOOXX"},{"OOOXX", "OOOXX", "OOOXX", "OOOXX", "OOOXX"},
                 {"OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"},{"OOOXX", "OOOXX", "OOOXX", "OOOXX", "OOOXX"}};
         problem81302.solution(test);
     }
 
     public int[] solution(String[][] places) {
         int[] answer = new int[5];
+        int index = 0;
         char[][] room = new char[5][5];
-        int col = 0;
-        int row_pivot = 0;
-        int col_pivot = 0;
-        int distance = 0;
-        int check = 1;
 
-        /*
-        * 각 강의실에 들어있는 값들을
-        * String -> char[] 형태로 변환
-        */
-        for(int i=0;i<places.length;i++) {
-            for (int j = 0; j < places[i].length; j++) {
-                room[j] = places[i][j].toCharArray();
-            }
-            Map<Integer, List<Integer>> store = new HashMap<>();
+        for(int i=0;i<5;i++) answer[i] = 1;
 
-
-            System.out.println("강의실 입장 !!");
-            for (int x = 0; x < 5; x++) {
-                for (int y = 0; y < 5; y++) {
-                    System.out.print(room[x][y] + " | ");
+        for (String[] place : places) {
+            for(int i=0;i<place.length;i++) {
+                for(int j=0;j<place[i].length();j++) {
+                    room[i][j] = place[i].charAt(j);
                 }
-                System.out.println();
             }
 
-            /*
-             * 응시자가 앉아있는 위치 좌표값을 저장한다
-             */
-            for (int x = 0; x < 5; x++) {
-                List<Integer> numStore = new ArrayList<>();
-                for (int y = 0; y < 5; y++) {
-                    if (room[x][y] == 'P') {
-                        numStore.add(y);
+            for(int i=0;i<5;i++) {
+                for(int j=0;j<5;j++) {
+                    char pivot = room[i][j];
+                    char right = 0;
+                    char bottom = 0;
+                    char rightCross = 0;
+                    char rightPlus = 0;
+                    char bottomPlus = 0;
+                    char leftCross = 0;
+                    char left = 0;
+
+
+                    if (i + 1 < 5) bottom = room[i+1][j];
+                    if (j + 1 < 5) {
+                        right = room[i][j+1];
+                        if (i + 1 < 5) rightCross = room[i+1][j+1];
                     }
-                }
-                store.put(x, numStore);
-            }
+                    if (i + 2 < 5) bottomPlus = room[i+2][j];
+                    if (j + 2 < 5) rightPlus = room[i][j+2];
+                    if (j >= 1) {
+                        left = room[i][j-1];
+                        if (i + 1 < 5) leftCross = room[i+1][j-1];
+                    }
 
-            /*
-             * 응시자중 한명을 기준으로 잡고,
-             * 각 응시자간의 맨해튼 거리를 계산한다.
-             */
-            System.out.println("스토어 값 : " + store);
-            for (Integer key : store.keySet()) {
-                System.out.println("현재 키값 : " + key);
-                check = 1;
-                if (store.isEmpty()) break;
-                row_pivot = key;
-                for (Integer key2 : store.keySet()) {
-                    if (store.get(key).isEmpty()) break;
-                    int row = key2;
-                    col_pivot = store.get(key).get(0);
-                    for (int count = 0; count < store.get(key2).size(); count++) {
-                        System.out.println("col 값 : " + store.get(key2).get(count));
-                        if ((row_pivot != row || col_pivot != store.get(key2).get(count))) {
-                            col = store.get(key2).get(count);
-                            distance = Math.abs(col - col_pivot) + Math.abs(row - row_pivot);
-                            System.out.println("[row_pivot, col_pivot] : [" + row_pivot + ", " + col_pivot + "]");
-                            System.out.println("[row, col] : [" + row + ", " + col + "]");
-                            System.out.println("distance : " + distance);
-                            if (distance == 1) {
-                                // 거리가 1인 경우에는 어떠한 경우라도 거리두기를 지키지 못함
-                                check = 0;
-                                break;
-                            } else if (distance <= 2) {
-                                // 거리가 2인 경우에는, 같은 행 또는 열 에있는지 파악 하거나 서로다른 행 또는 열에 있는지 파악
-                                if (col == col_pivot) {
-                                    int new_row = (row + row_pivot) / 2;
-                                    if (room[new_row][col] == 'O') {
-                                        // 같은 Col 사이에 'O' 가 존재하면 거리두기를 지키지 못함
-                                        check = 0;
-                                        break;
-                                    }
-                                } else if (row == row_pivot) {
-                                    int new_col = (col + col_pivot) / 2;
-                                    if (room[row][new_col] == 'O') {
-                                        // 같은 Row 사이에 'O' 가 존재하면 거리두기를 지키지 못함
-                                        check = 0;
-                                        break;
-                                    }
-                                } else {
-                                    if (room[row][col_pivot] == 'O' || room[row_pivot][col] == 'O') {
-                                        // 다른 Row, Col 사이에 빈 테이블이 하나라도 있다면 거리두기를 지키지 못함
-                                        check = 0;
-                                        break;
-                                    }
-                                }
-                            }
+                    if (pivot == 'P') {
+                        if ((right == 'O' || bottom == 'O') && rightCross == 'P') {
+                            answer[index] = 0;
+                            break;
                         }
+
+                        if((left == 'O' || bottom == 'O') && leftCross == 'P') {
+                            answer[index] = 0;
+                            break;
+                        }
+
+                        if (right == 'P' || bottom == 'P') {
+                            answer[index] = 0;
+                            break;
+                        }
+
+                        if ((right == 'O' && rightPlus == 'P') || (bottom == 'O' && bottomPlus == 'P')) {
+                            answer[index] = 0;
+                            break;
+                        }
+
                     }
-                    if (check == 0) break;
                 }
-                if (check == 0) break;
+                if (answer[index] == 0) {
+                    break;
+                }
             }
-            System.out.println("체크 값 " + check);
-            answer[i] = check;
-        }
-        for (int i : answer) {
-            System.out.println(i);
+            System.out.println("answer["+index+"] : " + answer[index]);
+            index++;
         }
         return answer;
     }
