71c98d6a24a37bf54e29a9c44a65c1f6e52019fc
coldexpression <uni_vers_@naver.com>
2022-10-16 23:00:13 +0900

    [정답 - 재풀이 필요] 131129번

--- 프로그래머스/src/Level3/problem131129.java
+++ 프로그래머스/src/Level3/problem131129.java
@@ -9,33 +9,49 @@ public class problem131129 {
     }
 
     public int[] solution(int target) {
-        HashMap<Integer, int[]> map = new HashMap<>();
+        int[][] dp = new int[target+1][target+1];
         int[] answer = new int[2];
-        for(int i=1;i<=60;i++) {
-            if (i <= 20) {
-                map.put(i, new int[]{1, 1});
-            } else if (i <= 40) {
-                if (i % 3 == 0) map.put(i, new int[]{1, 0});
-                else map.put(i, new int[]{2, 2});
-            } else if (i <= 49) {
-                if (i % 3 == 0) map.put(i, new int[]{1, 0});
-                else map.put(i, new int[]{2, 1});
-            } else if (i <= 59) {
-                if (i % 3 == 0) map.put(i, new int[]{1, 0});
-                map.put(i, new int[]{2, 2});
+
+        for(int i=1;i<=target;i++) {
+            if (i <= 20 || i == 50) {
+                dp[i][0] = 1;
+                dp[i][1] = 1;
+            } else if (i <= 60 && i % 3 == 0) {
+                dp[i][0] = 1;
+                dp[i][1] = 0;
+            } else if (i <= 40 && i % 2 == 0) {
+                dp[i][0] = 1;
+                dp[i][1] = 0;
+            } else if (i >= 51 && i <= 70) {
+                dp[i][0] = 2;
+                dp[i][1] = 2;
+            } else if (i < 70) {
+              if (i < 40) {
+                  dp[i][0] = 2;
+                  dp[i][1] = 2;
+              } else {
+                  dp[i][0] = 2;
+                  dp[i][1] = 1;
+              }
             } else {
-                map.put(i, new int[]{1, 0});
+                int a1 = dp[i-50][0];
+                int a2 = dp[i-50][1];
+                int b1 = dp[i-60][0];
+                int b2 = dp[i-60][1];
+                if (a1 < b1) {
+                    dp[i][0] = a1 + 1;
+                    dp[i][1] = a2 + 1;
+                } else if (a1 > b1) {
+                    dp[i][0] = b1 + 1;
+                    dp[i][1] = b2;
+                } else {
+                    dp[i][0] = a1+1;
+                    dp[i][1] = Math.max(a2+1,b2);
+                }
             }
         }
-
-        int mok = target / 60;
-        int rest = target % 60;
-
-        int[] ints = map.get(rest);
-
-        answer[0] = mok + ints[0];
-        answer[1] = ints[1];
-
+        answer[0] = dp[target][0];
+        answer[1] = dp[target][1];
         System.out.println("["+answer[0]+","+answer[1]+"]");
         return answer;
     }
