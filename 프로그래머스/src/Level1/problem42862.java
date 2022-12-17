fb4cef600368d13253526d42657b185a6a192473
coldexpression <uni_vers_@naver.com>
2022-12-17 12:39:37 +0900

    [정답 - 새로운 풀이] 42862번

--- 프로그래머스/src/Level1/problem42862.java
+++ 프로그래머스/src/Level1/problem42862.java
@@ -1,6 +1,8 @@
 package Level1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class problem42862 {
@@ -13,94 +15,36 @@ public class problem42862 {
 
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int temp = 0;
-        int cnt = 0;
-        int count = 0;
-        int new_lost_index = 0;
-        int new_reserve_index = 0;
-        int min = 0;
-        int[] new_lost = {};
-        int[] new_reserve = {};
-        List<Integer> store = new ArrayList<>();
+        HashSet<Integer> lostSet = new HashSet<>();
+        HashSet<Integer> reserveSet = new HashSet<>();
+        int[] pass = new int[n+1];
+        List<Integer> list = new ArrayList<>();
 
-        for (int i=0; i<reserve.length; i++) {
-            for (int j=0; j<lost.length; j++) {
-               if (reserve[i] == lost[j]) {
-                   reserve[i] = 0;
-                   lost[j] = 0;
-                   count++;
-                   break;
-               }
+        Arrays.stream(lost).forEach(ele -> lostSet.add(ele));
+        Arrays.stream(reserve).forEach(ele -> reserveSet.add(ele));
+
+        reserveSet.stream().filter(ele -> lostSet.contains(ele))
+                .forEach(ele -> list.add(ele));
+
+        list.forEach(ele -> lostSet.remove(ele));
+        list.forEach(ele -> reserveSet.remove(ele));
+
+        int idx = 0;
+        for (int stu : lostSet) {
+            if (stu == 1) pass[stu+1]++;
+            else if (stu == n) pass[stu-1]++;
+            else {
+                pass[stu+1]++;
+                pass[stu-1]++;
             }
         }
 
-        new_lost = new int[lost.length-count];
-        new_reserve = new int[reserve.length-count];
-
-        for (int i=0;i<reserve.length;i++) {
-            if (new_reserve_index == new_reserve.length) break;
-            if (reserve[i] != 0) {
-                new_reserve[new_reserve_index] = reserve[i];
-                new_reserve_index++;
+        for (int stu : reserveSet) {
+            if (pass[stu] != 0) {
+                answer++;
+                pass[stu] = 0;
             }
         }
-
-        for (int i=0;i<lost.length;i++) {
-            if (new_lost_index == new_lost.length) break;
-            if (lost[i] != 0) {
-                new_lost[new_lost_index] = lost[i];
-                new_lost_index++;
-            }
-        }
-
-
-        for (int i=0;i<new_lost.length-1;i++) {
-            min = new_lost[i];
-            for (int j=i;j<new_lost.length;j++) {
-                if (min > new_lost[j]) {
-                    temp = new_lost[j];
-                    new_lost[j] = min;
-                    min = temp;
-                }
-            }
-            new_lost[i] = min;
-        }
-
-        for (int i=0;i<new_reserve.length-1;i++) {
-            min = new_reserve[i];
-            for (int j=i;j<new_reserve.length;j++) {
-                if (min > new_reserve[j]) {
-                    temp = new_reserve[j];
-                    new_reserve[j] = min;
-                    min = temp;
-                }
-            }
-            new_reserve[i] = min;
-        }
-
-
-        for (int i=0;i<new_reserve.length;i++) {
-            for (int j=0;j<new_lost.length;j++) {
-                if (!store.contains(new_lost[j])) {
-                    if (new_reserve[i] == 1 && new_lost[j] == 2) {
-                        cnt++;
-                        store.add(new_lost[j]);
-                    } else if (new_reserve[i] == n && new_lost[j] == n - 1) {
-                        cnt++;
-                        store.add(new_lost[j]);
-                    } else if ((new_lost[j] == new_reserve[i] - 1 || new_lost[j] == new_reserve[i] + 1) && new_lost[j] != new_reserve[i]) {
-                        cnt++;
-                        store.add(new_lost[j]);
-                        break;
-                    }
-                }
-            }
-        }
-
-
-
-        answer = n - new_lost.length + cnt;
-
 
         return answer;
     }
