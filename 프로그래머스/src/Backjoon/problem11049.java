92191bd0a500b1c8bbba58087d64b5b38fa2c8c3
coldexpression <uni_vers_@naver.com>
2022-12-26 21:23:17 +0900

    [재풀이 필요 - 이해가 안됨] 11049번

--- 프로그래머스/src/Backjoon/problem11049.java
+++ 프로그래머스/src/Backjoon/problem11049.java
@@ -9,6 +9,7 @@ public class problem11049 {
 
         int N = sc.nextInt();
         int[][] alph = new int[N+1][2];
+        int[] v = new int[N+1];
         int answer = Integer.MAX_VALUE;
 
         for(int i=1;i<=N;i++) {
@@ -16,66 +17,29 @@ public class problem11049 {
             alph[i][1] = sc.nextInt();
         }
 
-        int[][] dp = new int[N+1][N+1];
-
-//        for(int i=1;i<=N;i++)
-//            for(int j=1;j<=N;j++)
-//                dp[i][j] = Integer.MAX_VALUE;
-
-        for(int i=1;i<N;i++) {
-            dp[2][i] = alph[i][0]*alph[i][1]*alph[i+1][1];
-        }
-
-        for(int i=3;i<=N;i++) {
-            System.out.println("i >> " + i);
-            for(int j=1;j<=N-i+1;j++) {
-                System.out.println("j >> " + j);
-                dp[i][j] = Integer.MAX_VALUE;
-
-                for(int k=0;k<=1;k++) {
-                    int temp = 0;
-                    System.out.println("k >> " + k);
-
-                    if (k == 0) {
-                        temp = dp[i-1][j];
-                        int sub = 1;
-
-                        for(int l=0;l<i;l++) {
-                            if (l == 0) sub = alph[j+l][0];
-                            else sub *= alph[j+l][1];
-                        }
-
-                        temp += sub;
-
-                        System.out.println("k == 0 => " + temp);
-
-                        dp[i][j] = Math.min(dp[i][j], temp);
-                    } else {
-                        temp = dp[i-1][j+1];
-                        int sub = 1;
-
-                        for(int l=0;l<i;l++) {
-                            if (l == i-1) sub *= alph[j+l][1];
-                            else sub *= alph[j+l][0];
-                        }
-
-                        temp += sub;
-
-                        System.out.println("k != 0 => " +temp);
-
-                        dp[i][j] = Math.min(dp[i][j], temp);
-                    }
-                }
-            }
-        }
-
         for(int i=1;i<=N;i++) {
-            for(int j=1;j<=N;j++) {
-                System.out.print("["+dp[i][j]+"]");
+            if (i == 1) {
+                v[0] = alph[1][0];
+                v[1] = alph[1][1];
+            } else {
+                v[i] = alph[i][1];
             }
-            System.out.println();
         }
 
-        System.out.println(dp[N][1]);
+        int[][] dp = new int[N][N];
+
+        for(int i=2;i<N+1;i++) {
+            for(int j=0;j<N-i+1;j++) {
+                dp[j][j+i-1] = Integer.MAX_VALUE;
+                for(int k=j;k<j+i-1;k++) {
+                    int temp = dp[j][k] + dp[k+1][j+i-1] + (v[j]*v[k+1]*v[j+i]);
+                    dp[j][j+i-1] = Math.min(dp[j][j+i-1], temp);
+                }
+
+            }
+        }
+
+
+        System.out.println(dp[0][N-1]);
     }
 }
