10cb4a7f5b6ffa30c1bac58638195236aca81839
coldexpression <uni_vers_@naver.com>
2023-05-13 19:13:08 +0900

    [정답] 2935번

---
+++ 프로그래머스/src/Backjoon/problem2935.java
@@ -0,0 +1,17 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class problem2935 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger a = new BigInteger(br.readLine());
+        String op = br.readLine();
+        BigInteger b = new BigInteger(br.readLine());
+        if (op.equals("*")) System.out.print(a.multiply(b));
+        else System.out.print(a.add(b));
+    }
+}
