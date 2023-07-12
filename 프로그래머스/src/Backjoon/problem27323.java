dc74ef53a03497a31dfd0135e4036e4ab26b7402
coldexpression <uni_vers_@naver.com>
2023-07-12 22:13:53 +0900

    [정답] 27323번

---
+++ 프로그래머스/src/Backjoon/problem27323.java
@@ -0,0 +1,15 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class problem27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(bf.readLine());
+        int b = Integer.parseInt(bf.readLine());
+        System.out.println(a*b);
+    }
+}
