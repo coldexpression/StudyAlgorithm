d61ab2a38df6dbae04bf24ed6f51e83a8bf6234f
coldexpression <uni_vers_@naver.com>
2023-09-07 10:33:31 +0900

    [정답] 10757번

---
+++ 프로그래머스/src/Backjoon/problem10757.java
@@ -0,0 +1,14 @@
+package Backjoon;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class problem10757 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+    }
+}
