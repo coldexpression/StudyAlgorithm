2e0f861f7aa2068eddce2cf6045ebfee0a38760c
coldexpression <uni_vers_@naver.com>
2023-11-26 14:57:12 +0900

    [정답] 15733번

---
+++ 프로그래머스/src/Backjoon/problem15733.java
@@ -0,0 +1,10 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class problem15733 {
+    public static void main(String[] args) {
+        System.out.println("I'm Sexy");
+    }
+}
