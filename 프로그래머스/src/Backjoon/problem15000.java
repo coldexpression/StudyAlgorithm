d600f921a353528c2b592287043599a83aee2254
coldexpression <uni_vers_@naver.com>
2023-12-04 22:08:47 +0900

    [정답] 15000번

---
+++ 프로그래머스/src/Backjoon/problem15000.java
@@ -0,0 +1,12 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class problem15000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(bf.readLine().toUpperCase());
+    }
+}
