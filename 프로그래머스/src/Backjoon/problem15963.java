ed50c0307ea540f440e4820b76216e166b5f8e27
coldexpression <uni_vers_@naver.com>
2023-12-09 15:35:43 +0900

    [정답] 15963번

---
+++ 프로그래머스/src/Backjoon/problem15963.java
@@ -0,0 +1,16 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class problem15963 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.println(n == m ? 1 : 0);
+    }
+}
