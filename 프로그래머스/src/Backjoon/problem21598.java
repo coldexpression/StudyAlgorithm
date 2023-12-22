da367708b4a9bec7fae4757055882a3d8eec2523
coldexpression <uni_vers_@naver.com>
2023-12-22 14:00:25 +0900

    [정답] 21598번

---
+++ 프로그래머스/src/Backjoon/problem21598.java
@@ -0,0 +1,13 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class problem21598 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++) sb.append("SciComLove").append("\n");
+        System.out.print(sb);
+    }
+}
