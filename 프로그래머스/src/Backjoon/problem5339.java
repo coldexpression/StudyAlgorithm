f99ea92ee4882460b3b5e15c993ecf6d91c3b615
coldexpression <uni_vers_@naver.com>
2024-01-03 21:05:12 +0900

    [정답] 5339번

---
+++ 프로그래머스/src/Backjoon/problem5339.java
@@ -0,0 +1,16 @@
+package Backjoon;
+
+public class problem5339 {
+    public static void main(String[] args) {
+        System.out.print("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
