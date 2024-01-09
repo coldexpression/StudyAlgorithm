58f364073d52b0329bfeb960ab23c809525fcb93
coldexpression <uni_vers_@naver.com>
2024-01-09 20:24:19 +0900

    [정답] 6778번

---
+++ 프로그래머스/src/Backjoon/problem6778.java
@@ -0,0 +1,25 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class problem6778 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        int Antena = sc.nextInt();
+        int eyes = sc.nextInt();
+
+
+        if (Antena >= 3 && eyes <=4){
+            System.out.println("TroyMartian");
+        }if(Antena <= 6 && eyes >= 2 ){
+            System.out.println("VladSaturnian");
+        }if(Antena <=2 && eyes <= 3){
+            System.out.println("GraemeMercurian");
+        }else {
+
+        }
+    }
+}
