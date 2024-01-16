ae915114628b3d49f62ed4e698d4678fc0348a77
coldexpression <uni_vers_@naver.com>
2024-01-16 21:17:33 +0900

    [정답] 13597번

---
+++ 프로그래머스/src/Backjoon/problem13597.java
@@ -0,0 +1,18 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class problem13597 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int number1 = Integer.parseInt(st.nextToken());
+        int number2 = Integer.parseInt(st.nextToken()); //(1)
+
+        if (number1 == number2) System.out.println(number1); //(2)
+        else System.out.println(number1 > number2 ? number1 : number2); //(3)
+    }
+}
