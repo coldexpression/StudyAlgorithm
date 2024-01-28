85f10832800e23c8878ac16b7d4d89804b3d1df4
coldexpression <uni_vers_@naver.com>
2024-01-28 20:49:25 +0900

    [정답] 1731번

---
+++ 프로그래머스/src/Backjoon/problem1731.java
@@ -0,0 +1,21 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class problem1731 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int[] n = new int[num];
+
+        for (int i = 0; i < n.length; i++) {
+            n[i] = scan.nextInt();
+        }
+        scan.close();
+
+        if (n[2] - n[1] == n[1] - n[0])
+            System.out.println(n[n.length - 1] + (n[1] - n[0]));
+        else
+            System.out.println(n[n.length - 1] * (n[1] / n[0]));
+    }
+}
