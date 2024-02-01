d1f81678e86406b72828e6438348963e8c256744
coldexpression <uni_vers_@naver.com>
2024-02-01 23:23:58 +0900

    [정답] 1834번

---
+++ 프로그래머스/src/Backjoon/problem1834.java
@@ -0,0 +1,16 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class problem1834 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long num = scan.nextLong();
+        long sum = 0;
+
+        for (int i = 0; i < num; i++) {
+            sum += (num + 1) * i;
+        }
+        System.out.println(sum);
+    }
+}
