24212b855065796a8e11a7be7c66997289428f7f
coldexpression <uni_vers_@naver.com>
2024-02-06 21:38:34 +0900

    [정답] 3004번

---
+++ 프로그래머스/src/Backjoon/problem3004.java
@@ -0,0 +1,36 @@
+package Backjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class problem3004 {
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException{
+        int A = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        bw.write(sb.append(printNum(A)).toString());
+        bw.close();
+    }
+
+
+    static int printNum(int num) {
+        if(num%2!=1) { 		// 짝수의 경우
+            num = num/2;
+            num = num+1;
+            num*=num;
+            return num;
+
+        }else { 			// 홀수의 경우
+
+            int row = ((num-1)/2)+1;
+            int col = ((num+1)/2)+1;
+
+            return row*col;
+        }
+    }
+}
