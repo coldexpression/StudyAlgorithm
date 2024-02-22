fb8e62b8de3499a1b2bf975ad80cdaae71a2c4f9
coldexpression <uni_vers_@naver.com>
2024-02-22 20:48:23 +0900

    [정답] 11006번

---
+++ 프로그래머스/src/Backjoon/problem11006.java
@@ -0,0 +1,20 @@
+package Backjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class problem11006 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println((b * 2 - a) + " " + (b - (b * 2 - a)));
+        }
+        sc.close();
+    }
+}
