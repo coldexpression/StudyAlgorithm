a79b1729a5104861320a0ccacfc4bdbb8bfcb100
coldexpression <uni_vers_@naver.com>
2024-02-26 22:05:21 +0900

    [정답] 5704번

---
+++ 프로그래머스/src/Backjoon/problem5704.java
@@ -0,0 +1,30 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+public class problem5704 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int[] alpabet = new int[26];
+            String str = sc.nextLine().replace(" ", "");
+            if (str.equals("*"))
+                break;
+            for (int i = 0; i < str.length(); i++) {
+                ++alpabet[str.charAt(i) - 97];
+            }
+            String ret = "Y";
+            for (int i = 0; i < alpabet.length; i++) {
+                if (alpabet[i] == 0) {
+                    ret = "N";
+                    break;
+                }
+            }
+            System.out.println(ret);
+        }
+
+        sc.close();
+    }
+}
