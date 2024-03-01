8d5d0077f290c17c3557237433b5a086aa57fcdb
coldexpression <uni_vers_@naver.com>
2024-03-01 19:33:10 +0900

    [정답] 14645번

---
+++ 프로그래머스/src/Backjoon/problem14645.java
@@ -0,0 +1,20 @@
+package Backjoon;
+
+import java.util.Scanner;
+
+/**
+ * @author Chansik Seo
+ */
+public class problem14645 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        for(int i=0;i<n;i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
