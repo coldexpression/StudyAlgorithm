63dd68d2d7e02f9a99b97a867a5b913eadba040b
coldexpression <uni_vers_@naver.com>
2024-03-19 20:36:47 +0900

    [정답] 24262번

---
+++ 프로그래머스/src/Backjoon/problem24262.java
@@ -0,0 +1,12 @@
+package Backjoon;
+
+/**
+ * @author Chansik Seo
+ */
+public class problem24262 {
+
+    public static void main(String[] args) {
+        System.out.println(0);
+        System.out.println(1);
+    }
+}
