224de82d98f122fe29fc2d79c91641e8b837abf1
coldexpression <uni_vers_@naver.com>
2024-03-31 21:45:09 +0900

    [정답] 11719번

---
+++ 프로그래머스/src/Backjoon/problem11719.java
@@ -0,0 +1,13 @@
+package Backjoon;
+
+import java.io.*;
+import java.util.*;
+public class problem11719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
