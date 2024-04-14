f36fb29224f10994f2d6126da8c792a5a4ced929
coldexpression <uni_vers_@naver.com>
2024-04-14 19:55:30 +0900

    [정답] 2010번

---
+++ 프로그래머스/src/Backjoon/problem2010.java
@@ -0,0 +1,16 @@
+package Backjoon;
+
+import java.io.*;
+
+public class problem2010 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            total += Integer.parseInt(br.readLine());
+        }
+        total -= (n-1);
+        System.out.print(total);
+    }
+}
